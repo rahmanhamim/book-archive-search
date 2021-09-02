6ed09daa4bae1403a4279b14e6f6651dc10afccd
rahmanhamim <rahmanhamiminfo@gmail.com>
2021-09-02 13:18:06 +0600

    final submit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 const loadBookData = () => {
  const searchField = document.getElementById("search-field");
  searchValue = searchField.value;
+ // spinner
+ toggleSpinner("block");
+
  // clear search value
  searchField.value = "";
+
  // fetch data
- fetch(`http://openlibrary.org/search.json?q=${searchValue}`)
+ fetch(`https://openlibrary.org/search.json?q=${searchValue}`)
   .then((res) => res.json())
-  .then((data) => displayResults(data.docs));
+  .then((data) => displayResults(data));
 };
 
 const displayResults = (books) => {
@@ -14,18 +18,19 @@ const displayResults = (books) => {
  const resultContainer = document.getElementById("result-container");
  // total result number container
  const totalResultNumber = document.getElementById("total-result");
-
  // clear inner HTML
  resultContainer.textContent = "";
  totalResultNumber.textContent = "";
 
  // check if result available
- if (books.length === 0) {
+ if (books.docs.length === 0) {
   resultContainer.innerHTML = `<h1 class='text-center text-danger mx-auto'>No result found</h1>`;
- } else {
-  // loop & append child
-  books.forEach((book) => {
-   // check cover image available or not
+ }
+
+ // loop & append child
+ else {
+  books.docs.forEach((book) => {
+   // check cover image available or not ---------------------
    if (book.cover_i === undefined) {
     const div = document.createElement("div");
     div.classList.add("col");
@@ -50,7 +55,8 @@ const displayResults = (books) => {
   `;
     resultContainer.appendChild(div);
    }
-   // if image is available
+
+   // if image is available ----------------------------
    else {
     const div = document.createElement("div");
     div.classList.add("col");
@@ -79,10 +85,21 @@ const displayResults = (books) => {
    }
   });
  }
- // total number of result
+
+ // total number of result --------------------------------
  const totalResultDiv = document.createElement("div");
  totalResultDiv.innerHTML = `
-      <h1 class="text-center text-danger my-5 shadow-text">Total Result Found: ${books.length}</h1>
+       <h1 class="text-center text-danger my-5 shadow-text">Result Showing: ${books.docs.length}</h1>
+      <h1 class="text-center text-danger my-5 shadow-text">Total Result Found: ${books.numFound}</h1>
  `;
  totalResultNumber.appendChild(totalResultDiv);
+
+ //spinner
+ toggleSpinner("none");
+};
+
+// spinner
+const toggleSpinner = (spinnerStyle) => {
+ const spinner = document.getElementById("spinner");
+ spinner.style.display = spinnerStyle;
 };
